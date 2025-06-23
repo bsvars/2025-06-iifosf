ca3c51f9331e0f61dcf66c64e202803aff88e9c8
adamwang15 <adamwang15@gmail.com>
2025-06-23 21:34:55 +1000

    R6 and S3

--- nicetry.R
+++ nicetry.R
@@ -25,31 +25,16 @@ sign_irf <- array(sign_irf, c(4, 4, 4)) # last for 4 periods
 sign_structural <- matrix(NA, 4, 4)
 sign_structural[1, ] <- c(1, -1, -1, 1)
 
+sign_irf[, , 1]
+
+sign_structural
+
 # specify the model
 spec <- specify_bsvarSIGN$new(
   Y,
   p = 4,
-  # exogenous = Z,
   sign_irf = sign_irf,
   sign_structural = sign_structural
 )
 
-# disable dummy observation priors
-spec$prior$Ysoc <- matrix(NA, nrow(spec$prior$Ysoc), 0)
-spec$prior$Xsoc <- matrix(NA, nrow(spec$prior$Xsoc), 0)
-spec$prior$Ysur <- matrix(NA, nrow(spec$prior$Ysur), 0)
-spec$prior$Xsur <- matrix(NA, nrow(spec$prior$Xsur), 0)
-
-# sample posterior draws
-post <- estimate(spec, S = 5000, show_progress = FALSE)
-
-# compute impulse response functions
-irf <- compute_impulse_responses(post, horizon = 20)
-rownames(irf) <- c("Cash Rate", "Real GDP", "CPI", "TWI")
-colnames(irf) <- c("Monetary Policy Shock", "shock 2", "shock 3", "shock aa")
-
-# plot(irf, probability = 0.68)
-# summary(irf)$shock1$variable2
-
-hd <- compute_historical_decompositions(post)
-plot(hd)
+round(spec$prior$A, 4)
